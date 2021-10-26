a71b801eb55a114d54a294d6b8da8e3d9be87ab4
HEHABUSTb <msanvold@gmail.com>
2021-10-26 19:10:54 +0300

    add py test fixtures

--- Scripts/Pytest/test_fixture2.py
+++ Scripts/Pytest/test_fixture2.py
@@ -17,11 +17,14 @@ def prepare_data():
     print("preparing some critical data for every test")
 
 class TestMainPage1():
+
     #Call fixture like parametr in test
+    @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
 
+    @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
